cb033d5d77944b6af87e323607ab71d9f52c1e07
Nicola Ananda Dwi Ervantoro <nicolaanandadwiervantoro@192.168.1.21>
2025-03-14 01:52:32 +0700

    django kedua

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], # add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- notes/views.py
+++ notes/views.py
@@ -4,5 +4,8 @@ from .models import Note
 # Create your views here.
 def index_view(request):
     name = "Nicola"
+    # email = "gmail@nicola.id"
     notes = Note.objects.all()
     return render(request, "index.html", {"name": name, "notes": notes})
+    # countries = ["Indonesia", "Malaysia", "Singapore", "Thailand", "Vietnam", "Philippines"]
+    # return render(request, "index.html", {"name": name, "countries": countries, "email": email})
